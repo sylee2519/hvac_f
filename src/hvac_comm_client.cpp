85d04031f2e77a00a92365d1efd6b3270ddb4ba8
sylee2519 <sylee2519@naver.com>
2024-07-27 00:37:24 +0900

    ver1 #10 (logdir)

--- src/hvac_comm_client.cpp
+++ src/hvac_comm_client.cpp
@@ -523,8 +523,10 @@ hg_addr_t hvac_client_comm_lookup_addr(int rank)
 	hg_addr_t target_server;
 	bool svr_found = false;
 	FILE *na_config = NULL;
-	sprintf(filename, "./.ports.cfg.%s", jobid);
+//	sprintf(filename, "./.ports.cfg.%s", jobid);
 //	sprintf(filename, "./.ports.cfg");
+	const char *logdir = getenv("HVAC_LOG_DIR");
+    snprintf(filename, sizeof(filename), "%s/.ports.cfg.%s", logdir, jobid);
 	na_config = fopen(filename,"r+");
    
 	if (na_config == NULL) {
